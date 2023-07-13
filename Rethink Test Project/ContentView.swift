143b612cfaabc0ee0091379521d11a4897571674
ziontuiasoa <ztuiasoa@icloud.com>
2023-07-12 18:24:45 -0600

    Initial Commit

---
+++ Rethink Test Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rethink Test Project
+//
+//  Created by Zion Tuiasoa on 7/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rethink Test Project/Rethink_Test_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rethink_Test_ProjectApp.swift
+//  Rethink Test Project
+//
+//  Created by Zion Tuiasoa on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Rethink_Test_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
